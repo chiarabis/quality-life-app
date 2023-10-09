23904a39b2415f1912b27faa18c35950e534f060
Chiara <124071052+chiarabis@users.noreply.github.com>
2023-10-09 14:58:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,50 +1,82 @@
-function formatText(input) {
-  let result = "";
-  
-  for (let i = 0; i < input.length; i++) {
-      if (input[i] === " ") {
-          result += "-";
-      } else {
-          result += input[i];
-      }
-  }
-  return result;
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded", function() {
-  const searchButton = document.querySelector(".searchButton");
+    const cityInfo = document.querySelector(".city-info");
+    const h2 = cityInfo.querySelector("h2");
+    const categoriesDiv = cityInfo.querySelector(".categories");
+    const description = cityInfo.querySelector(".description");
+    const cityScore = cityInfo.querySelector(".city-score");
+    categoriesDiv.innerHTML = "";
 
-  searchButton.addEventListener("click", function() {
-      const inputCityName = document.querySelector(".searchInput").value;
-      const input = formatText(inputCityName.toLowerCase());
-      const apiUrl = `https://api.teleport.org/api/urban_areas/slug:${input}/scores/`;
+    const apiUrl = `https://api.teleport.org/api/urban_areas/`;
 
-      fetch(apiUrl)
-          .then(response => response.json())
-          .then(data => {
-              const cityInfoDiv = document.querySelector(".cityInfo");
-              const h2 = cityInfoDiv.querySelector("h2");
-              const description = cityInfoDiv.querySelector(".description");
-              const cityScore = cityInfoDiv.querySelector(".city-score");
-              const categoriesDiv = cityInfoDiv.querySelector(".categories");
+    async function search(cityInput) {
+        try {
+            const response = await fetch(apiUrl);
 
-              categoriesDiv.innerHTML = "";
+            if (!response.ok) {
+                throw new Error("Error in the API request to search the cities");
+            }
 
-              data.categories.forEach(category => {
-                  const categoryDiv = document.createElement("div");
-                  categoryDiv.classList.add("category");
-                  categoryDiv.style.backgroundColor = category.color;
-                  categoryDiv.textContent = `${category.name}: ${category.score_out_of_10}`;
-                  categoriesDiv.appendChild(categoryDiv);
-              });
+            const data = await response.json();
+            const cities = data._links["ua:item"];
+            //const foundCity = cities.find((city) => city.name.toLowerCase() === cityInput);
+            
+            let foundCity = null;
 
-              h2.textContent = input.toUpperCase();
-              description.innerHTML = data.summary;
-              cityScore.textContent = `City score: ${data.teleport_city_score.toFixed(2)}`;
-              
-          })
-          .catch(error => {
-              console.error(error);
-          });
-      });
+            for (let i = 0; i < cities.length; i++) {
+                if (cities[i].name.toLowerCase() === cityInput) {
+                    foundCity = cities[i];
+                    break;
+                }
+            }
+
+            if (foundCity) {
+                return foundCity;
+            }
+            return alert("City not found or written incorrectly!");
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const searchButton = document.getElementById("searchButton");
+
+    searchButton.addEventListener("click", async function () {
+
+        categoriesDiv.innerHTML = "";
+        description.innerHTML = "";
+        cityScore.textContent = "";
+        
+        const cityInput = document.getElementById("searchInput").value.toLowerCase();
+        let city = "";
+
+        try {
+            const result = await search(cityInput);
+            city = result;
+        } catch (error) {
+            console.error(error);
+        }
+
+        fetch(`${city.href}scores/`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Error in the API request to search the scores");
+            }
+            return response.json();
+        })
+        .then((dataCity) => {
+            dataCity.categories.forEach((category) => {
+                const categoryDiv = document.createElement("div");
+                categoryDiv.classList.add("category");
+                categoryDiv.style.backgroundColor = category.color;
+                categoryDiv.textContent = `${category.name}: ${category.score_out_of_10}`;
+                categoriesDiv.appendChild(categoryDiv);
+            });
+
+            h2.textContent = city.name;
+            description.innerHTML = dataCity.summary;
+            cityScore.textContent = `City score: ${dataCity.teleport_city_score.toFixed(2)}`;
+        });
+    });
 });
